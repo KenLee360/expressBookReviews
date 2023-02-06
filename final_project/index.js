d4eb162f05b761ea73cfa0a156a3b562e89be2f7
KenLee360 <kennylee360@gmail.com>
2023-02-05 20:11:41 -0500

    Pushing Node Final Project

--- final_project/index.js
+++ final_project/index.js
@@ -11,8 +11,22 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+    if(req.session.authorization) {
+            toekn = req.session.authoriztion['accessToken'];
+            jwt.verify(toekn, "access", (err,user)=> {
+                if(!err){
+                    req.user = user;
+                    next();
+                }
+                else{
+                    return res.status(403).json({message: "User not Authenticated"})
+                }
+            });
+        }
+        else{
+            return res.status(403).json({message: "User not logged in"})
+        }
+    });
  
 const PORT =5000;
 
